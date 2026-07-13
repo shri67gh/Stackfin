2e1f5159a08bc7d6cd1b20e9a8ad861e54ee7bfc
shri67gh <shristi.110105@gmail.com>
2026-07-13 20:43:30 +0530

    stock project

---
+++ frontend/src/landing_page/signup/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Hero from '../home/Hero';
+function SignUp() {
+    return ( 
+        <div>
+            <Hero/>
+        </div>
+        
+     );
+}
+
+export default SignUp;
---
+++ frontend/src/landing_page/support/Hero.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="container-fluid p-0" id="supportHero">
+      <div className="p-5 " id="supportWrapper">
+        <h4>Support Portal</h4>
+        <a href="">Track Tickets</a>
+      </div>
+      <div className="row p-6 m-5 d-flex justify-content-between">
+        <div className="col-5 p-2">
+          <h1 className="fs-5">
+            Search for an answer or browse help topics to create a ticket
+          </h1><br></br>
+          <input placeholder="Eg. how do I activate F&O" style={{maxWidth:"500px"}} />
+          <br /><br></br>
+          <a href="">Track account opening</a>&nbsp;&nbsp;
+          <a href="">Track segment activation</a>&nbsp;&nbsp;
+          <a href="">Intraday margins</a>&nbsp;&nbsp;
+          <a href="">Kite user manual</a>&nbsp;&nbsp;
+        </div>
+        <div className="col-6 p-3">
+          <h1 className="fs-3">Featured</h1>
+          <ol>
+            <li>
+              <a href="">Current Takeovers and Delisting - January 2024</a>
+            </li>
+            <li>
+              <a href="">Latest Intraday leverages - MIS & CO</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
